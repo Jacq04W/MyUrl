1c912f294d65a2576b82960482ab8e58a0368092
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-09-05 13:04:58 -0400

    Initial Commit

---
+++ MyUrl/MyUrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyUrlApp.swift
+//  MyUrl
+//
+//  Created by Jacquese Whitson on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MyUrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
